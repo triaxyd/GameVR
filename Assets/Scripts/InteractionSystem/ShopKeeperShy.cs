3a19832f23b8befaa45b70f45c5c564606963d00
triaxyd <triantafyllosxyd@gmail.com>
2024-11-16 17:03:42 +0200

    second character

---
+++ Assets/Scripts/InteractionSystem/ShopKeeperShy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopKeeperShy : MonoBehaviour, IInteractable
+{
+    public string InteractionPrompt => throw new System.NotImplementedException();
+
+    public bool Interact(Interactor interactor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
